588d89f0cb4358a891777527cd11a570579d57ff
wrightadebayo <wrightadebayo80@gmail.com>
2025-06-05 17:01:28 +0100

    updated-1

---
+++ tests/getfilterbyText.spec.js
@@ -0,0 +1,11 @@
+const { test, expect } = require('@playwright/test');
+
+test('launch application', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.getByPlaceholder('username').fill('standard_user');
+  await page.getByPlaceholder('password').fill('secret_sauce');
+  await page.getByRole('button').click();
+
+  await page.locator('xpath=//*[@class="inventory_item"]').filter({hasText:'Sauce Labs Bike Light'}).getByRole('button',{name:'Add to cart'}).click()
+  await page.waitForTimeout(10000);
+});
---
+++ tests/getfilterbychild.spec.js
@@ -0,0 +1,39 @@
+const { test, expect } = require('@playwright/test');
+
+test('launch application', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.getByPlaceholder('username').fill('standard_user');
+  await page.getByPlaceholder('password').fill('secret_sauce');
+  await page.getByRole('button').click();
+
+  await page
+    .locator('xpath=//*[@class="inventory_item"]')
+    .filter({ has: page.getByRole('link',{name:'Sauce Labs Backpack '}) })
+    .getByRole('button', { name: 'Add to cart' })
+    .click();
+  await page.waitForTimeout(10000);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
