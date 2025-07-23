95f76e4aa1a56b5f3a2ccdf6e39459a20a0a5321
wrightadebayo <wrightadebayo80@gmail.com>
2025-07-24 00:02:46 +0100

    Run drag and drop test in CI

--- tests/filter.spec.js
+++
@@ -1,6 +0,0 @@
-const {test, expect} = require('@playwright/test')
-
-test('launch application', async({page})=>{
-await page.goto('https://www.saucedemo.com/inventory.html');
-await page.getByRole('link', {name: 'Forgot login info?'}).click();
-})
--- tests/fixturescontext.spec.js
+++ tests/fixturescontext.spec.js
@@ -1,15 +1,15 @@
 const { test, expect } = require('@playwright/test');
 
-test('fixtures and context', async ({ browser }) => {
+//test('fixtures and context', async ({ browser }) => {
   
-  const context = await browser.newContext({baseURL:'https://www.google.com'})
-   const page = await context.newPage()
-   await page.goto('/search?q=test')
-});
+//   const context = await browser.newContext({baseURL:'https://www.google.com'})
+//    const page = await context.newPage()
+//    await page.goto('/search?q=test')
+// });
 
-test('Test application', async({page})=>{
-  await page.goto('https://nairaland.com');
-})
+// test('Test application', async({page})=>{
+//   await page.goto('https://nairaland.com');
+// })
 
 
 test('testing on multiple windows and tabs', async ({ browser }) => {
@@ -23,14 +23,14 @@ test('testing on multiple windows and tabs', async ({ browser }) => {
   await page.locator('(//img)[3]').click(),
   ])
  await newPage.locator('//button[@aria-label="Install"]').click()
- await page.waitForTimeout(3000)
+
  await newPage.locator("(//button[contains(text(),'Cancel')])[2]").click()
- await page.waitForTimeout(3000)
+ //await page.waitForTimeout(3000)
   await newPage.close()
 await page.locator("(//input[@id='name'])[1]").fill('playwright tutor')
-await page.waitForTimeout(3000)
+//await page.waitForTimeout(3000)
 await page.locator("(//input[@id='email'])[1]").fill('wrightadebayo80@gmail.com')
-await page.waitForTimeout(3000)
+//await page.waitForTimeout(3000)
  await page.getByRole('link',{name:'Get Access'}).click()
  await page.waitForTimeout(3000)
  await page.close()
